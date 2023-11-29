6ac849b14c3172885a8bfe5d75b29e3548bb06ee
Jason Todd <jason@bluntstick.com>
2023-11-29 10:25:55 -0500

    Part 2

--- 2022/1/ConsoleApplication1.cpp
+++ 2022/1/ConsoleApplication1.cpp
@@ -12,17 +12,26 @@ int main() {
     if (new_file.is_open()) {
         string sa;
 
-        int max = 0;
+        int max[3];
+        for (int i = 0; i < 3; i++) {
+            max[i] = 0;
+        }
         int current = 0;
 
         // Read data from the file object and put it into a string.
         while (getline(new_file, sa)) {
             // Print the data of the string.
-            
+
             if (sa.compare("") == 0) {
                 cout << ">>LINEBREAK" << std::endl;
-                if (max < current) {
-                    max = current;
+                int spot = -1;
+                for (int i = 0; i < 3; i++) {
+                    if (max[i] < current && (spot == -1 || max[i] <= max[spot])) {
+                        spot = i;
+                    }
+                }
+                if (spot != -1) {
+                    max[spot] = current;
                 }
                 current = 0;
             }
@@ -30,11 +39,15 @@ int main() {
                 int currCalorie = stoi(sa);
                 current += currCalorie;
             }
-
-
-
         }
-        cout << "MAX:" << max << std::endl;
+
+        int total = 0;
+        for (int i = 0; i < 3; i++) {
+            cout << "MAX:" << i << ":" << max[i] << std::endl;
+            total += max[i];
+        }
+        cout << "TOTAL:" << total << std::endl;
+
         // Close the file object.
         new_file.close();
     }
